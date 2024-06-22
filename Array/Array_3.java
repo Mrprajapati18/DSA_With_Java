805ea0f36fd625e7814cba20963b3fa1a7c84d4e
Durgesh Prajapati <durgeshkumar778080@gmail.com>
2024-06-23 00:52:28 +0530

    commit

---
+++ Array/Array_3.java
@@ -0,0 +1,29 @@
+
+public class Array_3 {
+  void multiArrays()
+  {
+    // int [] [] arr = new int [5] [3];
+    int [] [] arr = {{56,86,90},{4,5,6},{99,64,67}}; // 2D Array
+
+    //arr [0] = Array ,,arr[0][2];  
+
+    System.out.println(arr[0][0]);
+    System.out.println(arr[0][1]);
+    System.out.println(arr[0][2]);
+
+    System.out.println(arr[1][0]);
+    System.out.println(arr[1][1]);
+    System.out.println(arr[1][2]);
+
+    System.out.println(arr[2][0]);
+    System.out.println(arr[2][1]);
+    System.out.println(arr[2][2]);
+  }
+  public static void main(String[] args) {
+    Array_3 obj = new Array_3();
+    obj.multiArrays();
+      
+  }
+}
+     
+
