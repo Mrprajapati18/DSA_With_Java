4245ac5aa929037630b56acbdab14526b734a121
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-18 13:34:32 +0530

    Commit

--- Array/StdMarks.java
+++ Array/StdMarks.java
@@ -13,9 +13,16 @@ public class StdMarks {
     
     marks[2] = marks[2] + 1; //  updated marks
 
+
     System.out.println("Physics Marks :" + marks[0]);
     System.out.println("Chemestry Marks: " + marks[1]);
     System.out.println("English marks: " + marks[2]);
-    
+
+    int percentage  = (marks[0] + marks[1] + marks[2]) / 3;
+    System.out.println("Percentage = " + percentage + "%");
+
+
+    System.out.println("Length of Array is " + marks.length); // Check the lenght of Array
+    sc.close();
   }
 }
