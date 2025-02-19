bb8f478b882f1e02404ae0e949cad00f9a69daba
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-20 03:28:59 +0530

    Sum 2D Array in 3D Array

--- Day_4_3D_Array/Sum_2D_In_3DArray.java
+++ Day_4_3D_Array/Sum_2D_In_3DArray.java
@@ -5,12 +5,15 @@ public class Sum_2D_In_3DArray {
   public static void main(String[] args) {
     
     Scanner Scan = new Scanner(System.in);
+    System.out.println("Enter the number of depth: ");
     int depth = Scan.nextInt();
+    System.out.println("Enter the number of rows: ");
     int rows = Scan.nextInt();
+    System.out.println("Enter the number of cols: ");
     int cols = Scan.nextInt();
     // Create 3D array
     int array[][][] = new int[depth][rows][cols];
-
+    System.out.println("Enter the elements of the 3D Array");
     // Populate the Array with user input
     for(int i = 0; i < depth; i++) {
       for(int j = 0; j < rows; j++) {
@@ -19,5 +22,27 @@ public class Sum_2D_In_3DArray {
         }
       }
     }
+    // Display the 3D Array
+    System.out.println("3D Array: ");
+    for(int i = 0; i < depth; i++) {
+      System.out.println("Layer " + (i + 1) + ": ");
+      for(int j = 0; j < rows; j++) {
+        for(int k = 0; k < cols; k ++){
+        System.out.print(array[i][j][k] + " ");
+        }
+        System.out.println();
+      }
+    }
+    // Calculate and print the each 2D Array
+    for(int i = 0; i < depth; i++) {
+      int sum = 0;
+      for(int j = 0; j < rows; j++) {
+        for(int k = 0; k < cols; k++) {
+          sum += array[i][j][k];
+        }
+      }
+      System.out.println("Sum Of the layer " + (i + 1) + ": " + sum);
+    }
+    Scan.close();
   }
 }
