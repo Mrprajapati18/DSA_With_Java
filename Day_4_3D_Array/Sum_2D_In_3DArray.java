c31d703479bcf8644e3e5acd276f4ee5529a65db
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-20 03:06:58 +0530

    commit

--- Day_4_3D_Array/Sum_2D_In_3DArray.java
+++ Day_4_3D_Array/Sum_2D_In_3DArray.java
@@ -10,6 +10,14 @@ public class Sum_2D_In_3DArray {
     int cols = Scan.nextInt();
     // Create 3D array
     int array[][][] = new int[depth][rows][cols];
-    
+
+    // Populate the Array with user input
+    for(int i = 0; i < depth; i++) {
+      for(int j = 0; j < rows; j++) {
+        for(int k = 0; k < cols; k++) {
+          array[i][j][k] = Scan.nextInt();
+        }
+      }
+    }
   }
 }
