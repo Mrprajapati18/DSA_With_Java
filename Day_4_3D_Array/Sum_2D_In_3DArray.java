9ce8f4414a4d3d76f8202d48c3852563944c045d
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-20 02:53:08 +0530

    commit

--- Day_4_3D_Array/Sum_2D_In_3DArray.java
+++ Day_4_3D_Array/Sum_2D_In_3DArray.java
@@ -5,7 +5,10 @@ public class Sum_2D_In_3DArray {
   public static void main(String[] args) {
     
     Scanner Scan = new Scanner(System.in);
-
-    // 
+    int depth = Scan.nextInt();
+    int rows = Scan.nextInt();
+    int cols = Scan.nextInt();
+    // Create 3D array
+    int array[][][] = new int[depth][rows][cols];
   }
 }
