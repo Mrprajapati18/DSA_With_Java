147c85d530060863b3b6a1d49bccfa8bba7c576d
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-21 13:42:02 +0530

    Trim and Split String

--- Day_5_String/StringManipulation1.java
+++ Day_5_String/StringManipulation1.java
@@ -14,5 +14,22 @@ public class StringManipulation1 {
       else { 
           System.out.println("Both strings are same"); 
       } 
-  } 
+      System.out.println("==========================");
+      String S = "    KODNEST";
+      System.out.println(S);
+      String str = S.trim();  // To Remove the extra spaces
+      System.out.println(str);
+      System.out.println("===========================");
+      String S1 = "KODNEST TECHNOLOGIES PVT LTD";
+      String arr[] = S1.split(" ");
+      for (String X : arr) {
+          System.out.println(X);
+       }  
+       System.out.println("==========================");
+       String S2 = "KODNEST TECHNOLOGIES PVT LTD";
+      String arr1[] = S2.split("O");
+      for (String Y : arr1) {
+          System.out.println(Y);
+       }  
+   } 
 }
