c800e7b52a69beed8c19e89d5b827948e3aeff35
MrPrajapati18 <durgeshkumar778080@gmail,com>
2025-02-23 17:38:15 +0530

    Commit

--- Day_4_3D_Array/Avg_3D_Array.java
+++ Day_4_3D_Array/Avg_3D_Array.java
@@ -14,6 +14,7 @@ public class Avg_3D_Array {
    int cols = Scan.nextInt();
 
    // Create the 3D Array
+   //=======================================
    int array[][][] = new int[depth][rows][cols];
 
    // populate the Array with user input using loop
